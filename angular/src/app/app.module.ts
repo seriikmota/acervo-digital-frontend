aa6365226687184406742ee4d506e7220ba117a5
Davii-Code <davifarigoncalves@gmail.com>
2024-09-03 20:39:57 -0300

    UEG-44 generics angular

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GlobalsTsComponent } from './shared/globals.ts.component';
-import { AbstractDatasourceComponent } from './shared/abstract-datasource.component';
 import { AbstractListarComponent } from './shared/abstract-listar/abstract-listar.component';
 import { AbstractEditarComponent } from './shared/abstract-editar/abstract-editar.component';
 import { AbstractDataSourceComponent } from './shared/abstract-data-source.component';
@@ -13,8 +11,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    GlobalsTsComponent,
-    AbstractDatasourceComponent,
     AbstractListarComponent,
     AbstractEditarComponent,
     AbstractDataSourceComponent
