2957e23f015e25ada041b378c63710711883d6c0
davif <davifariagoncalves@gmail.com>
2024-11-22 18:18:39 -0300

    UEG-83

--- angular/src/app/feature-post/edit-post/edit-post.component.ts
+++ angular/src/app/feature-post/edit-post/edit-post.component.ts
@@ -1,8 +1,21 @@
-import {Component, Inject} from '@angular/core';
+import {Component, inject, Inject} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {PostService} from "../post.service";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-
+import {SecurityService} from "../../architecture/security/security.service";
+import {postRoles} from "../post-routing.module";
+type PermissionConfig = {
+  HAS_PERMISSION_CREATE?: boolean,
+  HAS_PERMISSION_UPDATE?: boolean,
+  HAS_PERMISSION_DELETE?: boolean,
+  HAS_PERMISSION_READ?: boolean,
+};
+export type RoleConfig = {
+  CREATE_ROLE?: string,
+  UPDATE_ROLE?: string,
+  DELETE_ROLE?: string,
+  READ_ROLE?: string,
+};
 @Component({
   selector: 'app-edit-post',
   templateUrl: './edit-post.component.html',
@@ -12,19 +25,47 @@ export class EditPostComponent {
   editPostForm: FormGroup;
   selectedFile: File | null = null;
   currentIndex: number = 0;
+  permissionConfig: PermissionConfig;
 
   constructor(
     private fb: FormBuilder,
     private postService: PostService,
     private dialogRef: MatDialogRef<EditPostComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private securityService: SecurityService
   ) {
+    // Inicializando as permissões
+    this.permissionConfig = this.getPermissions();
+
+    // Criando o formulário com as permissões
     this.editPostForm = this.fb.group({
-      title: [this.data?.title || '', [Validators.required]],
-      subtitle: [this.data?.subtitle || '', [Validators.required]],
-      content: [this.data?.content || '', [Validators.required]],
-      tag: [this.data?.tag || '', [Validators.required]]
+      title: [{ value: this.data?.title || '', disabled: !this.permissionConfig.HAS_PERMISSION_UPDATE }, [Validators.required]],
+      subtitle: [{ value: this.data?.subtitle || '', disabled: !this.permissionConfig.HAS_PERMISSION_UPDATE }, [Validators.required]],
+      content: [{ value: this.data?.content || '', disabled: !this.permissionConfig.HAS_PERMISSION_UPDATE }, [Validators.required]],
+      tag: [{ value: this.data?.tag || '', disabled: !this.permissionConfig.HAS_PERMISSION_UPDATE }, [Validators.required]],
     });
+  }
+
+  // Retorna a configuração de roles
+  private getRoles(): RoleConfig {
+    // Aqui você deve definir os roles de acordo com sua lógica ou carregar de algum serviço
+    return {
+      CREATE_ROLE: postRoles.CREATE,
+      UPDATE_ROLE: postRoles.UPDATE ,
+      DELETE_ROLE: postRoles.DELETE ,
+      READ_ROLE:  postRoles.READ,
+    };
+  }
+
+  // Retorna as permissões do usuário com base nos roles
+  private getPermissions(): PermissionConfig {
+    const roles = this.getRoles();  // Obtendo os roles definidos acima
+    return {
+      HAS_PERMISSION_CREATE: this.securityService.hasRoles(roles.CREATE_ROLE || ''),
+      HAS_PERMISSION_UPDATE: this.securityService.hasRoles(roles.UPDATE_ROLE || ''),
+      HAS_PERMISSION_DELETE: this.securityService.hasRoles(roles.DELETE_ROLE || ''),
+      HAS_PERMISSION_READ: this.securityService.hasRoles(roles.READ_ROLE || ''),
+    };
   }
 
   onFileSelected(event: any): void {
